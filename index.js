cccd2dd1ec2939a75f5256a0ebcb9a0ce524bce5
flyingdutchman03 <tiesb2.0@gmail.com>
2026-03-13 23:08:04 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+[
+  {
+    "name": "Nyaa",
+    "id": "nyaa",
+    "version": "0.0.1",
+    "type": "anime",
+    "accuracy": "high",
+    "icon": "https://nyaa.si/static/favicon.png",
+    "update": "https://raw.githubusercontent.com/flyingdutchman03/hayase-extensions/main/index.json",
+    "code": "https://raw.githubusercontent.com/flyingdutchman03/hayase-extensions/main/nyaa.js"
+  }
+]
